f639d9747d1fc1d474610ed6a658cb7131866f4b
Cosiamo <61847214+Cosiamo@users.noreply.github.com>
2022-03-11 15:03:31 -0600

    added user input

--- main.go
+++ main.go
@@ -7,8 +7,13 @@ import (
 )
 
 func main() {
+	var input string
+
+	fmt.Println("What do you want to search?")
+	fmt.Scanln(&input)
+
 	// GoogleScrape(searchTerm, countryCode, languageCode, proxyString, pages, count, backoff)
-	res, err := logic.GoogleScrape("github", "com", "en", nil, 1, 30, 10)
+	res, err := logic.GoogleScrape(input, "com", "en", nil, 1, 30, 10)
 	// if no error, range over res var and print a response
 	if err  == nil {
 		for _, res := range res {
