d2e29dda4601d26bd2d8cd5dc18d75c1dcb30c8b
Cosiamo <61847214+Cosiamo@users.noreply.github.com>
2022-03-14 16:45:54 -0500

    added user input

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # TODO
-- Fix user input so that it detects spaces 
-    - tried looking up "fried chicken recipe" but it returned definitions for "fried"
-    - in `buildGoogleUrls` it should be replacing " " with "+" so that the Google query can detect it
-    - possibly need to rework how the variables work in logic and main packages
 - Run program with `sea` keyword
 - Allow users to change settings
     - change domain, language, number of requests, and proxy
     - use a flag such as `-s` or `-settings`
+- Add Bing Scraper
+- let user choose between Google or Bing
--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	logic "github.com/Cosiamo/SeaUrchin/logic"
 )
 
 func main() {
-	var input string
+    input := make([]string, 0)
 
-	fmt.Println("What do you want to search?")
-	fmt.Scanln(&input)
+    scanner := bufio.NewScanner(os.Stdin)
+
+    fmt.Print("What do you want to search? : ")
+
+    scanner.Scan()
+    text := scanner.Text()
+	input = append(input, text)
+
+	refinedInput := strings.Join(input, "+")
 
 	// GoogleScrape(searchTerm, countryCode, languageCode, proxyString, pages, count, backoff)
-	res, err := logic.GoogleScrape(input, "com", "en", nil, 1, 30, 10)
+	res, err := logic.GoogleScrape(refinedInput, "com", "en", nil, 1, 30, 10)
 	// if no error, range over res var and print a response
 	if err  == nil {
 		for _, res := range res {
