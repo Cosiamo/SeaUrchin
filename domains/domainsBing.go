99d5a0a90bb1f734ada2ff3ee6aa470054c8e1bf
Cosiamo <61847214+Cosiamo@users.noreply.github.com>
2022-03-15 13:23:03 -0500

    fixed spelling error in file name

---
+++ domains/domainsBing.go
@@ -0,0 +1,79 @@
+package domains
+
+// list of all available Bing domains
+var BingDomains = map[string]string {
+	// global
+	"com": "",
+	// United Kingdom
+	"uk": "&cc=GB",
+	// United States
+	"us": "&cc=US",
+	// Turkey
+	"tr": "&cc=TR",
+	// Tanzania, United Republic of
+	"tw": "&cc=TW",
+	// Switzerland
+	"ch": "&cc=CH",
+	// Sweden
+	"se": "&cc=SE",
+	// Spain
+	"es": "&cc=ES",
+	// South Africa
+	"za": "&cc=ZA",
+	// Saudi Arabia
+	"sa": "&cc=SA",
+	// Russia
+	"ru": "&cc=RU",
+	// The Philippines
+	"ph": "&cc=PH",
+	// Portugal
+	"pt": "&cc=PT",
+	// Poland
+	"pl": "&cc=PL",
+	// China
+	"cn": "&cc=CN",
+	// Norway
+	"no": "&cc=NO",
+	// New Zealand
+	"nz": "&cc=NZ",
+	// The Netherlands
+	"nl": "&cc=NL",
+	// Mexico
+	"mx": "&cc=MX",
+	// Malaysia
+	"my": "&cc=MY",
+	// South Korea
+	"kr": "&cc=KR",
+	// Japan
+	"jp": "&cc=JP",
+	// Italy
+	"it": "&cc=IT",
+	// Indonesia
+	"id": "&cc=ID",
+	// India
+	"in": "&cc=IN",
+	// Hong Kong
+	"hk": "&cc=HK",
+	// Germany
+	"de": "&cc=DE",
+	// France
+	"fr": "&cc=FR",
+	// Finland
+	"fi": "&cc=FI",
+	// Denmark
+	"dk": "&cc=DK",
+	// Chile
+	"cl": "&cc=CL",
+	// Canada
+	"ca": "&cc=CA",
+	// Brazil
+	"br": "&cc=BR",
+	// Belgium
+	"be": "&cc=BE",
+	// Austria
+	"at": "&cc=AT",
+	// Australia
+	"au": "&cc=AU",
+	// Argentina
+	"ar": "&cc=AR",
+}
