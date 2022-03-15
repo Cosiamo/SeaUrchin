9b21946345a57def22ca079764e53f765541f801
Cosiamo <61847214+Cosiamo@users.noreply.github.com>
2022-03-15 16:36:10 -0500

    refactored for cmd package

--- main.go
+++ main.go
@@ -8,14 +8,10 @@ import (
 	"os"
 	"strings"
 
-	// internal packages
-	logic "github.com/Cosiamo/SeaUrchin/logic"
+	cmd "github.com/Cosiamo/SeaUrchin/cmd"
 )
 
 func main() {
-	var GoogleInput string
-	var BingInput string
-
 	// sets user input
     input := make([]string, 0)
 
@@ -27,55 +23,16 @@ func main() {
     text := scanner.Text()
 	input = append(input, text)
 
-	// converts the string slices to a single string and concats them with "+"
+	// converts the string slices to a single string and concats them with a space
 	refinedInput := strings.Join(input, " ")
 
-	// subcommands - flags
 	// Google
 	GoogleCmd := flag.NewFlagSet("g", flag.ExitOnError)
-	g_search := GoogleCmd.String("", refinedInput, "Search on Google")
-	// converts pointer (*string) to normal value (string)
-	GoogleInput = *g_search
+	cmd.GoogleCommand(refinedInput, GoogleCmd)
 
 	// Bing
 	BingCmd := flag.NewFlagSet("b", flag.ExitOnError)
-	b_search := BingCmd.String("", refinedInput, "Search on Bing")
-	// converts pointer (*string) to normal value (string)
-	BingInput = *b_search
+	cmd.BingCommand(refinedInput, BingCmd)
 
-	// Switch between Google or Bing depending on flag
-	switch os.Args[1] {
-	// Google case
-	case "g":
-		GoogleCmd.Parse(os.Args[2:])
-		// GoogleScrape(searchTerm, countryCode, languageCode, proxyString, pages, count, backoff)
-		res, err := logic.GoogleScrape(GoogleInput, "com", "en", nil, 1, 30, 10)
-		// if no error, range over res var and print a response
-		if err  == nil {
-			for _, res := range res {
-				fmt.Println(res)
-			}
-		} else {
-			// a lot of projects use 'log' for errors
-			fmt.Println(err)
-		}
-	// Bing case
-	case "b":
-		BingCmd.Parse(os.Args[2:])
-		// BingScrape(searchTerm, country, proxyString, pages, count, backoff)
-		res, err := logic.BingScrape(BingInput, "com", nil, 1, 30, 10)
-		// if no error, range over res var and print a response
-		if err == nil {
-			for _, res := range res {
-				fmt.Println(res)
-			}
-		} else {
-			// a lot of projects use 'log' for errors
-			fmt.Println(err)
-		}
-	// default case
-	default:
-		fmt.Println("Expected g subcommand")
-		os.Exit(1)
-	}
+	cmd.SwitchAndCase(GoogleCmd, BingCmd)
 }
