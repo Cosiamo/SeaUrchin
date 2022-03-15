b64ac67f140afc335976f18c32ea71fe5f9ca2fb
Cosiamo <61847214+Cosiamo@users.noreply.github.com>
2022-03-15 14:48:00 -0500

    users can choose between Google or Bing

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 # How To Use
 - `go run main.go g` for Google search
-- If you run main.go without the `g` subcommand/flag it will exit with error
+- `go run main.go b` for Bing search
+- If you run main.go without the `g` or `b` subcommand/flag it will exit with error
     - this is intentional for right now
     - I plan on allowing users to set default search engines so you don't have to type in a flag every time
 
@@ -11,5 +12,3 @@
 - Allow users to change settings
     - change domain, language, number of requests, and proxy
     - use a flag such as `-s` or `-settings`
-- Add Bing Scraper
-- let user choose between Google or Bing
--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 
 func main() {
 	var GoogleInput string
+	var BingInput string
 
 	// sets user input
     input := make([]string, 0)
@@ -27,15 +28,22 @@ func main() {
 	input = append(input, text)
 
 	// converts the string slices to a single string and concats them with "+"
-	refinedInput := strings.Join(input, "+")
+	refinedInput := strings.Join(input, " ")
 
 	// subcommands - flags
+	// Google
 	GoogleCmd := flag.NewFlagSet("g", flag.ExitOnError)
 	g_search := GoogleCmd.String("", refinedInput, "Search on Google")
-
 	// converts pointer (*string) to normal value (string)
 	GoogleInput = *g_search
 
+	// Bing
+	BingCmd := flag.NewFlagSet("b", flag.ExitOnError)
+	b_search := BingCmd.String("", refinedInput, "Search on Bing")
+	// converts pointer (*string) to normal value (string)
+	BingInput = *b_search
+
+	// Switch between Google or Bing depending on flag
 	switch os.Args[1] {
 	// Google case
 	case "g":
@@ -47,12 +55,27 @@ func main() {
 			for _, res := range res {
 				fmt.Println(res)
 			}
+		} else {
+			// a lot of projects use 'log' for errors
+			fmt.Println(err)
+		}
+	// Bing case
+	case "b":
+		BingCmd.Parse(os.Args[2:])
+		// BingScrape(searchTerm, country, proxyString, pages, count, backoff)
+		res, err := logic.BingScrape(BingInput, "com", nil, 1, 30, 10)
+		// if no error, range over res var and print a response
+		if err == nil {
+			for _, res := range res {
+				fmt.Println(res)
+			}
+		} else {
+			// a lot of projects use 'log' for errors
+			fmt.Println(err)
 		}
 	// default case
 	default:
 		fmt.Println("Expected g subcommand")
 		os.Exit(1)
 	}
-
-
 }
