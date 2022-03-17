5f6d3a31deff3ea009446818b396bff888644f37
Cosiamo <61847214+Cosiamo@users.noreply.github.com>
2022-03-17 12:48:55 -0500

    moved user input out of main func

--- cmd/switch.go
+++ cmd/switch.go
@@ -1,22 +1,46 @@
 package cmd
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	logic "github.com/Cosiamo/SeaUrchin/logic"
 )
+
+var SeType string
 
 // Switch between Google or Bing depending on subcommand
 func SwitchAndCase(GoogleCmd *flag.FlagSet, BingCmd *flag.FlagSet) {
 	switch os.Args[1] {
 		// Google case
 		case "g":
-			GoogleCmd.Parse(os.Args[2:])
+			SeType := "Google"
+			
+			// sets user input
+			input := make([]string, 0)
 
+			// reading user input as a set of lines
+			scanner := bufio.NewScanner(os.Stdin)
+
+			fmt.Print("Search on ", SeType, ": ")
+
+			// advancing NewScanner to generate a token
+			scanner.Scan()
+			// returns the token as a newly allocated string
+			text := scanner.Text()
+			// appending text, string, to input, string slice
+			input = append(input, text)
+
+			// converts the string slice to a single string and concats them with a space
+			refinedInput := strings.Join(input, " ")
+
+
+			GoogleCmd.Parse(os.Args[2:])
 			// GoogleScrape(searchTerm, countryCode, languageCode, proxyString, pages, count, backoff)
-			res, err := logic.GoogleScrape(GoogleInput, "com", "en", nil, 1, 30, 10)
+			res, err := logic.GoogleScrape(refinedInput, "com", "en", nil, 1, 30, 10)
 
 			// if no error, range over res var and print a response
 			if err  == nil {
@@ -30,11 +54,30 @@ func SwitchAndCase(GoogleCmd *flag.FlagSet, BingCmd *flag.FlagSet) {
 
 		// Bing case
 		case "b":
+			SeType := "Bing"
+
+			// sets user input
+			input := make([]string, 0)
+		
+			// reading user input as a set of lines
+			scanner := bufio.NewScanner(os.Stdin)
+		
+			fmt.Print("Search on ", SeType, ": ")
+		
+			// advancing NewScanner to generate a token
+			scanner.Scan()
+			// returns the token as a newly allocated string
+			text := scanner.Text()
+			// appending text, string, to input, string slice
+			input = append(input, text)
+		
+			// converts the string slice to a single string and concats them with a space
+			refinedInput := strings.Join(input, " ")
+		
+
 			BingCmd.Parse(os.Args[2:])
-
 			// BingScrape(searchTerm, country, proxyString, pages, count, backoff)
-			res, err := logic.BingScrape(BingInput, "com", nil, 1, 30, 10)
-
+			res, err := logic.BingScrape(refinedInput, "com", nil, 1, 30, 10)
 			// if no error, range over res var and print a response
 			if err == nil {
 				for _, res := range res {
--- main.go
+++ main.go
@@ -1,42 +1,17 @@
 package main
 
 import (
-	// go packages
-	"bufio"
 	"flag"
-	"fmt"
-	"os"
-	"strings"
 
 	cmd "github.com/Cosiamo/SeaUrchin/cmd"
 )
 
 func main() {
-	// sets user input
-    input := make([]string, 0)
-
-	// reading user input as a set of lines
-    scanner := bufio.NewScanner(os.Stdin)
-
-    fmt.Print("What do you want to search? : ")
-
-	// advancing NewScanner to generate a token
-    scanner.Scan()
-	// returns the token as a newly allocated string
-    text := scanner.Text()
-	// appending text, string, to input, string slice
-	input = append(input, text)
-
-	// converts the string slice to a single string and concats them with a space
-	refinedInput := strings.Join(input, " ")
-
 	// Google
 	GoogleCmd := flag.NewFlagSet("g", flag.ExitOnError)
-	cmd.GoogleCommand(refinedInput, GoogleCmd)
 
 	// Bing
 	BingCmd := flag.NewFlagSet("b", flag.ExitOnError)
-	cmd.BingCommand(refinedInput, BingCmd)
 
 	// switches between Google or Bing depending on which subcommand the user inputs
 	cmd.SwitchAndCase(GoogleCmd, BingCmd)
