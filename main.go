265eb626d1129a8628f2a27a25bf2080776ce994
Cosiamo <61847214+Cosiamo@users.noreply.github.com>
2022-03-18 14:49:09 -0500

    started settings, need to finish

--- main.go
+++ main.go
@@ -11,7 +11,9 @@ func main() {
 	GoogleCmd := flag.NewFlagSet("g", flag.ExitOnError)
 	// Bing
 	BingCmd := flag.NewFlagSet("b", flag.ExitOnError)
+	// settings
+	SettingsCmd := flag.NewFlagSet("settings", flag.ExitOnError)
 
 	// switches between Google or Bing depending on which subcommand the user inputs
-	cmd.SwitchAndCase(GoogleCmd, BingCmd)
+	cmd.SwitchAndCase(GoogleCmd, BingCmd, SettingsCmd)
 }
