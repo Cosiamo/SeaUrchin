ac52a0876af5cf1ef5aa328b76d976f054aa138d
Cosiamo <61847214+Cosiamo@users.noreply.github.com>
2022-03-23 18:23:52 -0500

    moved functions for displaying supported domains

--- cmd/displayInfo.go
+++ cmd/displayInfo.go
@@ -2,10 +2,13 @@ package cmd
 
 import (
 	"fmt"
+	"html/template"
+	"os"
 	"strconv"
 	"strings"
 
 	build "github.com/Cosiamo/SeaUrchin/build"
+	domains "github.com/Cosiamo/SeaUrchin/domains"
 )
 
 func DisplayGoogleInfo(searchTerm string, domain string, backoff int) {
@@ -31,3 +34,43 @@ func DisplayBingInfo(searchTerm string, domain string, backoff int) {
 	link := strings.Join(url, "")
 	fmt.Println(link)
 }
+
+func GoogleDomainList() {
+	t := template.Must(template.New("").Funcs(template.FuncMap{
+		"isEven": isEven,
+	}).Parse(templ))
+
+	// list of Google domains in domainsGoogle file
+	m := domains.GoogleDomainsList
+
+	if err := t.Execute(os.Stdout, m); err != nil {
+		panic(err)
+	}
+}
+
+func BingDomainList() {
+	t := template.Must(template.New("").Funcs(template.FuncMap{
+		"isEven": isEven,
+	}).Parse(templ))
+
+	// list of Bing domains in domainsBing file
+	m := domains.BingDomainsList
+
+	if err := t.Execute(os.Stdout, m); err != nil {
+		panic(err)
+	}
+}
+
+func isEven() func() bool {
+	e := false
+	return func() bool {
+		e = !e
+		return e
+	}
+}
+
+// template for displaying the list of domains
+const templ = `{{$e := isEven}}
+{{- range $k, $v := . -}}
+    {{$k}}: {{$v}}
+{{end}}`
