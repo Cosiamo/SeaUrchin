c340fc0379b38bdb615d8c3dc09bb073fe297963
Cosiamo <61847214+Cosiamo@users.noreply.github.com>
2022-03-31 10:59:57 -0500

    added a help flag

--- cmd/switch.go
+++ cmd/switch.go
@@ -13,18 +13,24 @@ var searchTerm, domain string
 var backoff int
 
 // Switch between Google or Bing depending on subcommand
-func SwitchAndCase(GoogleCmd *flag.FlagSet, BingCmd *flag.FlagSet, SettingsCmd *flag.FlagSet, DomainsCmd *flag.FlagSet) {
+func SwitchAndCase(GoogleCmd *flag.FlagSet, BingCmd *flag.FlagSet, DomainsCmd *flag.FlagSet) {
 	switch os.Args[1] {
 		// Google case
 		case "g":
+			proxy := GoogleCmd.String("proxy", "", "Add a proxy string to your search")
+			changeDomain := GoogleCmd.String("url", "com", "Choose which Google domain you want to use ('domains' subcommand lists supported domains)")
+			showInfo := GoogleCmd.Bool("info", false, "Displays the backoff time and the URL that the results were searched from")
+			help := GoogleCmd.Bool("help", false, "Lists flag options")
+			GoogleCmd.Parse(os.Args[2:])
+
+			if *help {
+				GoogleCmd.PrintDefaults()
+				return
+			}
+			
 			fmt.Print("Search on Google: ")
 			searchTerm = logic.Input()
 			backoff = logic.Backoff()
-
-			proxy := GoogleCmd.String("proxy", "", "Add a proxy string to your search")
-			changeDomain := GoogleCmd.String("url", "com", "Choose which Google domain you want to use")
-			showInfo := GoogleCmd.Bool("info", false, "Displays the backoff time and the URL that the results were searched from")
-			GoogleCmd.Parse(os.Args[2:])
 
 			proxyString := client.ConnectProxy(proxy)
 			domain = *changeDomain
@@ -37,14 +43,20 @@ func SwitchAndCase(GoogleCmd *flag.FlagSet, BingCmd *flag.FlagSet, SettingsCmd *
 			}
 		// Bing case
 		case "b":
+			proxy := BingCmd.String("proxy", "", "Add a proxy string to your search")
+			changeDomain := BingCmd.String("url", "com", "Choose which Bing domain you want to use ('domains' subcommand lists supported domains)")
+			showInfo := BingCmd.Bool("info", false, "Displays the backoff time and the URL that the results were searched from")
+			help := BingCmd.Bool("help", false, "Lists flag options")
+			BingCmd.Parse(os.Args[2:])
+
+			if *help {
+				BingCmd.PrintDefaults()
+				return
+			}
+
 			fmt.Print("Search on Bing: ")
 			searchTerm = logic.Input()
 			backoff = logic.Backoff()
-
-			proxy := BingCmd.String("proxy", "", "Add a proxy string to your search")
-			changeDomain := BingCmd.String("url", "com", "Choose which Bing domain you want to use")
-			showInfo := BingCmd.Bool("info", false, "Displays the backoff time and the URL that the results were searched from")
-			BingCmd.Parse(os.Args[2:])
 
 			proxyString := client.ConnectProxy(proxy)
 			domain = *changeDomain
@@ -61,7 +73,7 @@ func SwitchAndCase(GoogleCmd *flag.FlagSet, BingCmd *flag.FlagSet, SettingsCmd *
 			DomainsCmd.Parse(os.Args[2:])
 
 			if !*showGoogleDomains && !*showBingDomains {
-				fmt.Println("To view the list of supported domains use the flag '-g' for Google or '-b' for Bing")
+				DomainsCmd.PrintDefaults()
 				return
 			}
 			if *showGoogleDomains {
@@ -72,26 +84,13 @@ func SwitchAndCase(GoogleCmd *flag.FlagSet, BingCmd *flag.FlagSet, SettingsCmd *
 				BingDomainList()
 				return
 			}
-		case "settings":
-			// googleDomainSet := SettingsCmd.String("gdomain", "", "Change the Google domain to your region")
-			// bindDomainSet := SettingsCmd.String("bdomain", "com", "Change the Bing domain to your region")
-			// SettingsCmd.Parse(os.Args[2:])
-
-			// if len(*googleDomainSet) > 0 {
-			// 	domainSet = *googleDomainSet
-			// }
-			
-
 		// if user inputs invalid subcommand
 		default:
 			fmt.Println("To search, use the subcommands:")
-			fmt.Println("'g' for Google")
-			fmt.Println("'b' for Bing")
-			fmt.Println("Use the '-info' flag to see the backoff time and the link that was used to fetch the results")
-			fmt.Println("Use the '-url' flag to change the region you're searching in")
-			fmt.Println("To view the supported regions use the subcommand 'domains' with the '-g' or '-b' flag")
-			// use this for when settings are implemted for their various flags
-			flag.PrintDefaults()
+			fmt.Println("	'g' for Google")
+			fmt.Println("	'b' for Bing")
+			fmt.Println("To view the supported regions use the subcommand:")
+			fmt.Println("	'domains' with the '-g' or '-b' flag")
 			os.Exit(1)
 		}
 }
