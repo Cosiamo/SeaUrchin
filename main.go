1f61cd144ca4098f4ec96001c5b9e0aec483576b
Cosiamo <61847214+Cosiamo@users.noreply.github.com>
2022-03-31 10:57:32 -0500

    removed settings subcommand

--- main.go
+++ main.go
@@ -11,11 +11,9 @@ func main() {
 	GoogleCmd := flag.NewFlagSet("g", flag.ExitOnError)
 	// Bing
 	BingCmd := flag.NewFlagSet("b", flag.ExitOnError)
-	// settings
-	SettingsCmd := flag.NewFlagSet("settings", flag.ExitOnError)
 	// lists supported domains
 	DomainsCmd := flag.NewFlagSet("domains", flag.ExitOnError)
 
 	// switches between Google or Bing depending on which subcommand the user inputs
-	cmd.SwitchAndCase(GoogleCmd, BingCmd, SettingsCmd, DomainsCmd)
+	cmd.SwitchAndCase(GoogleCmd, BingCmd, DomainsCmd)
 }
