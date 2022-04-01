7d8e7847851445c3c64ee5a3d0bf1da99b2a77f9
Cosiamo <61847214+Cosiamo@users.noreply.github.com>
2022-04-01 13:48:05 -0500

    if user does not input command, returns a list of options

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"os"
 
 	cmd "github.com/Cosiamo/SeaUrchin/cmd"
 )
@@ -14,6 +15,20 @@ func main() {
 	// lists supported domains
 	DomainsCmd := flag.NewFlagSet("domains", flag.ExitOnError)
 
-	// switches between Google or Bing depending on which subcommand the user inputs
-	cmd.SwitchAndCase(GoogleCmd, BingCmd, DomainsCmd)
+	// if user does not input subcommand,
+	// returns list of supported subcommands
+	if len(os.Args) < 2 {
+        cmd.DefaultCase()
+    }
+
+	switch os.Args[1] {
+	case "g":
+		cmd.GoogleCase(GoogleCmd)
+	case "b":
+		cmd.BingCase(BingCmd)
+	case "domains":
+		cmd.DomainsCase(DomainsCmd)
+	default:
+		cmd.DefaultCase()
+	}
 }
